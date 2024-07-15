dd8c917e9772aa49568a5831991c3d462adb1dee
AmniAbo <157361452+AmniAbo@users.noreply.github.com>
2024-07-15 17:12:27 +0300

    Add files via upload

---
+++ ClassWork/terg5/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Gallery from './Gallery';
+//import { FormatAlignCenter } from '@material-ui/icons';
+function App() {
+  return (
+    <>
+    <h1 style={{textAlign:'center'}}>Tutorial 5</h1>
+    <Gallery/>
+    </>
+  );
+}
+
+export default App;
